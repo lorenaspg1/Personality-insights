cb26ed3a30ae486eba03208eeb635d351a86f94e
lorenaspg1 <lorenaspg1@gmail.com>
2020-11-02 16:50:19 +0100

    Codigo refactorizado en funciones,  desacoplado archivos.txt del codigo

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from ibm_watson import PersonalityInsightsV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
-import csv
-
 from pathlib import Path
+from os.path import join, dirname
 
 
 authenticator = IAMAuthenticator('O3scp3HLHGiMK56gn2bxgx4chJoWZg_MSEANWL8-wdek')
@@ -13,23 +12,52 @@ personality_insights = PersonalityInsightsV3(
 
 personality_insights.set_service_url('https://api.eu-gb.personality-insights.watson.cloud.ibm.com/instances/f1b658b9-0d36-4f21-993e-123430f02866')
 
-with open('/Users/lorenaspg1/profile.txt') as profile_txt:
-    profile = personality_insights.profile(
-        profile_txt.read(),
-        accept="text/csv",
-        content_type='text/plain',
-        content_language='es',
-        accept_language='es',
-        raw_scores=True,
-        csv_headers=True,
-        consumption_preferences=True,
-    ).get_result()
+# Ruta haciia la carpeta contenedora del archivo . txt
+DIRECTORY_PATH = '/Users/lorenaspg1/profiles/'
 
-responseCsv = profile.content
+# Simboliza los archivos que se van a enviar a Watson- ir añadiendo a la carpeta archivos.txt
+files = ['profile.txt']
 
-Path('./myCSV.csv').touch()
+# Aqui se guardan los resultados HTTP Response de Watson
+watson_responses = []
 
-myFile = open('./myCSV.csv', 'w')
-with myFile:
-    myFile.write(responseCsv.decode())
+# Ejecuta la peticion a Watson y guarda los resultados en la lista profiles
+def get_results():
+    for file in files:
+        with open(join(dirname(DIRECTORY_PATH), file)) as profile_txt:
+            profile = personality_insights.profile(
+                    profile_txt.read(),
+                    accept="text/csv",
+                    content_type='text/plain',
+                    content_language='es',
+                    accept_language='es',
+                    raw_scores=True,
+                    csv_headers=True,
+                    consumption_preferences=True,
+                ).get_result()
+            watson_responses.append(profile)
 
+
+get_results()
+
+# Lista de CSV's devueta por Watson
+csvs = []
+
+# Obtenemos el contenido de la respuesta de Watson, el CSV en bytes
+def get_response_content():
+    for response in watson_responses:
+        csvs.append(response.content)
+
+
+get_response_content()
+
+def create_csv():
+    for i in range(0, len(csvs)):
+        path = "./myCSV{}.csv".format(i)
+        Path(path).touch()
+        myFile = open(path, 'w')
+        with myFile:
+            myFile.write(csvs[i].decode())
+
+
+create_csv()
